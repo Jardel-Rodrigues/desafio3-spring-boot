ee8f7681ac4d965a35a73a86d87638056bee90be
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2024-01-09 00:08:40 -0300

    Implemantado o metodo findAll para busca paginada.

--- src/main/java/com/softstrem/services/ClientService.java
+++ src/main/java/com/softstrem/services/ClientService.java
@@ -1,10 +1,13 @@
 package com.softstrem.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.softstrem.dto.ClientDTO;
+import com.softstrem.entities.Client;
 import com.softstrem.repositores.ClientRepository;
 import com.softstrem.services.exceptions.ResourceNotFoundException;
 
@@ -20,4 +23,10 @@ public class ClientService {
 				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
 	}
 
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(Pageable pageable){
+		Page<Client> result = repository.findAll(pageable);
+		return result.map(x -> new ClientDTO(x));
+	}
+	
 }
